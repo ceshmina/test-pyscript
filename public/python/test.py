c3a03ffd744c882eff7a30449c6db773a85c44e6
ceshmina <ceshmina@gmail.com>
2022-07-03 11:45:25 +0900

    numpy, pandasを使ってみる

--- public/python/test.py
+++ public/python/test.py
@@ -1,6 +1,22 @@
+import numpy as np
+import pandas as pd
+
+
 print('Hello PyScript!')
 
 a = 2
 b = 3
 c = a + b
 print(c)
+
+
+def f(x):
+  return x ** 2
+
+
+arr = np.array([1, 2, 3])
+print(f(arr))
+
+arr2 = np.array([2, 4, 6])
+df = pd.DataFrame({'a': arr, 'b': arr2})
+print(df)
